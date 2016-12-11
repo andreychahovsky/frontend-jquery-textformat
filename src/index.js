5f9f1a841935943c36ab6e2b2c4b6baf9f86ac75
andreychahovsky <chahovsky.andrey@outlook.com>
2016-12-11 23:35:55 +0200

    +copy text to the another area +toLowerCase

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
 'use strict';
 
 console.log($)
+
+// function that formating the text
+function formater(text) {
+	var text, lower;
+	var arr = [];
+	var i = 0;
+	var result;
+
+	lower = text.toLowerCase();
+	arr[i] = lower;
+	console.log(arr[i]);
+	i++;
+
+	result = arr;
+	return result;
+}
+
+
+// function for transfer text from one area to the other area using the function on the text
+function transferText() {
+	var notFormattedText, formattedText;
+	notFormattedText = $("textarea.message-field").val();
+	formattedText = formater(notFormattedText);
+	$("output.message-formatted").html(formattedText);
+}
+
+// function events on lost the focus
+$("textarea.message-field").blur(function () {
+	transferText();
+});
+
+// function events on keydown
+$("textarea.message-field").keydown(function () {
+	transferText();
+});
